fb541e007ab756b4232e989da9aea16e807b9852
sancarbar <sancarbar@gmail.com>
2018-07-29 11:53:16 -0500

    Implemented select photo from gallery and upload image to Firebase storage.

--- app/src/main/java/co/sancarbar/hampi/ui/activity/NewEntryActivity.kt
+++ app/src/main/java/co/sancarbar/hampi/ui/activity/NewEntryActivity.kt
@@ -5,14 +5,18 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.support.v4.content.FileProvider
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import co.sancarbar.hampi.R
+import com.google.firebase.storage.FirebaseStorage
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_new_entry.*
 import kotlinx.android.synthetic.main.new_entry_form.*
 import ui.utils.DialogFactory
@@ -27,12 +31,18 @@ import java.util.*
  * 7/24/18.
  */
 
-val REQUEST_TAKE_PHOTO = 1
+val TAKE_PHOTO_REQUEST_CODE = 85
+
+val SELECT_IMAGE_REQUEST_CODE = 86
 
 class NewEntryActivity : AppCompatActivity() {
 
 
-    lateinit var currentPhotoPath: String
+    lateinit var currentPhotoUri: Uri
+
+    private var storage: FirebaseStorage = FirebaseStorage.getInstance("gs://hampi-m.appspot.com")
+
+    private var storageRef = storage.getReference()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,7 +60,7 @@ class NewEntryActivity : AppCompatActivity() {
         val listener = DialogInterface.OnClickListener { dialog, which ->
 
             when (which) {
-                0 -> openGallery()
+                0 -> startPhotoSelectingFromExistingSources()
                 1 -> dispatchTakePictureIntent()
             }
             dialog.dismiss()
@@ -60,10 +70,24 @@ class NewEntryActivity : AppCompatActivity() {
             DialogFactory.showSingleChoiceDialog(this, R.array.add_photo_options_array,
                     R.string.Select_one_option, listener)
         }
+
+        save.setOnClickListener {
+            uploadImage()
+        }
     }
 
-    private fun openGallery() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun startPhotoSelectingFromExistingSources() {
+
+        val pictureIntent = Intent()
+        if (Build.VERSION.SDK_INT < 19) {
+            pictureIntent.action = Intent.ACTION_GET_CONTENT
+        } else {
+            pictureIntent.action = Intent.ACTION_OPEN_DOCUMENT
+            pictureIntent.addCategory(Intent.CATEGORY_OPENABLE)
+        }
+        pictureIntent.type = "image/*"
+        val intent = Intent.createChooser(pictureIntent, getString(R.string.Choose_from_library))
+        startActivityForResult(intent, SELECT_IMAGE_REQUEST_CODE)
     }
 
     private fun dispatchTakePictureIntent() {
@@ -76,7 +100,7 @@ class NewEntryActivity : AppCompatActivity() {
                 photoFile = createImageFile()
                 val photoURI: Uri = FileProvider.getUriForFile(this, "co.sancarbar.hampi.fileprovider", photoFile)
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+                startActivityForResult(takePictureIntent, TAKE_PHOTO_REQUEST_CODE)
             } catch (ex: IOException) {
                 // Error occurred while creating the File
 
@@ -85,11 +109,17 @@ class NewEntryActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == Activity.RESULT_OK) {
-//            val bitmap = BitmapFactory.decodeFile(currentPhotoPath)
-//            imageView.setImageBitmap(bitmap)
-//            imageView.visibility = View.VISIBLE
-            setPic()
+        if (resultCode == Activity.RESULT_OK) {
+
+            when (requestCode) {
+                TAKE_PHOTO_REQUEST_CODE -> updatePictureFromPhotoFile()
+
+                SELECT_IMAGE_REQUEST_CODE -> {
+                    imageView.setImageURI(data!!.data)
+                    currentPhotoUri = data.data
+                }
+            }
+
         }
     }
 
@@ -102,7 +132,7 @@ class NewEntryActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    private fun setPic() {
+    private fun updatePictureFromPhotoFile() {
         imageView.visibility = View.VISIBLE
         // Get the dimensions of the View
         val targetW = imageView.width
@@ -111,7 +141,7 @@ class NewEntryActivity : AppCompatActivity() {
         // Get the dimensions of the bitmap
         val bmOptions = BitmapFactory.Options()
         bmOptions.inJustDecodeBounds = true
-        BitmapFactory.decodeFile(currentPhotoPath, bmOptions)
+        BitmapFactory.decodeFile(currentPhotoUri.toString(), bmOptions)
         val photoW = bmOptions.outWidth
         val photoH = bmOptions.outHeight
 
@@ -124,7 +154,7 @@ class NewEntryActivity : AppCompatActivity() {
         if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.LOLLIPOP) {
             bmOptions.inPurgeable = true
         }
-        val bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions)
+        val bitmap = BitmapFactory.decodeFile(currentPhotoUri.path, bmOptions)
         imageView.setImageBitmap(bitmap)
     }
 
@@ -141,8 +171,52 @@ class NewEntryActivity : AppCompatActivity() {
                 storageDir      /* directory */
         )
 
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.absolutePath
+        currentPhotoUri = Uri.fromFile(image)
         return image
+    }
+
+    private fun uploadImage() {
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val photoRef = storageRef.child("/plants-images/$timeStamp")
+
+        val uploadTask = photoRef.putFile(currentPhotoUri)
+
+        uploadTask.continueWithTask { task ->
+            if (!task.isSuccessful) {
+                throw task.exception!!
+            }
+            photoRef.downloadUrl
+        }.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val downloadUri = task.result
+                Log.d("Developer", "DownloadUri $downloadUri")
+                Picasso.get().load(downloadUri).into(imageView)
+
+            } else {
+                DialogFactory.showInfoDialog(this, R.string.Upload_photo_error, R.string.Unable_to_upload_your_photo_Please_try_again)
+            }
+        }
+
+
+//        imageView.buildDrawingCache()
+//        val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+//        val byteArrayOutputStream = ByteArrayOutputStream()
+//        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+//        val data = byteArrayOutputStream.toByteArray()
+//
+//        val uploadTask = photoRef.putBytes(data)
+//
+//
+//
+//
+//
+//        uploadTask.addOnFailureListener {
+//
+//        }
+//        uploadTask.addOnSuccessListener  {taskSnapshot -> taskSnapshot.
+//
+//        }
+
+
     }
 }
